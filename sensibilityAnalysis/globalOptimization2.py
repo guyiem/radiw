778bae9c81580ca3c3490e94e9b3d87fc16ed7b5
guilhem <guilhem@sivienn.com>
2019-05-31 16:20:46 +0200

    ajout globalOptimization2

--- sensibilityAnalysis/globalOptimization2.py
+++ sensibilityAnalysis/globalOptimization2.py
@@ -3,6 +3,7 @@
 
 from scipy import optimize
 import numpy as npy
+from multiprocessing import Pool
 from scipy import linalg
 from math import pi,e
 from matplotlib import pyplot as plt
@@ -12,13 +13,33 @@ import sys
 sys.path.append('../')
 from ModeleVertical import *
 
+# goal of the script :
+# we load correlation radius we compute on the LHCS, we take the "closer" one to the experimental results and we use them with local optimization routine to find solutions to our optimization problem.
+
 pcapteurs = 50 - 0.015*npy.arange(0,640)[::-1] # position of the hydrophones
 dc =  0.015*npy.arange(0,640) # all the distance possible between the hydrophones
 
+# loading experimental correlation radius
 RCE = pickle.load(open("donnees/rce_sac2016.pick","rb"))
 RCE = npy.array([[*RCE]])
-Freqs = [ 2000 , 5000 , 7000 , 9000 , 11000 , 13000 ]
+# end of loading experimental correlation radius
 
+
+# loading of correlation radius
+Freqs = [ 2000 , 5000 , 7000 , 9000 , 11000 , 13000 ]
+nm = "11"
+rc2000 = pickle.load(open("donnees/rc1000e_m"+nm+"_2kHz.pick","rb"))
+rc5000 = pickle.load(open("donnees/rc1000e_m"+nm+"_5kHz.pick","rb"))
+rc7000 = pickle.load(open("donnees/rc1000e_m"+nm+"_7kHz.pick","rb"))
+rc9000 = pickle.load(open("donnees/rc1000e_m"+nm+"_9kHz.pick","rb"))
+rc11000 = pickle.load(open("donnees/rc1000e_m"+nm+"_11kHz.pick","rb"))
+rc13000 = pickle.load(open("donnees/rc1000e_m"+nm+"_13kHz.pick","rb"))
+RCB = npy.array([ rc2000 , rc5000 , rc7000 , rc9000 , rc11000 , rc13000 ])
+# end loading of correlation radius
+
+
+# loading sampling
+# WARNING : bounds parameters of the meta-model have to be enter by hands.
 pmin = npy.array([ 1530 , 1650 , 0.5 , 0.0001 , 5 , 50 ] )
 pmax = npy.array([ 1630 , 1750 , 2.0 , 0.005 , 50 , 150 ] )
 ne = "1000"
@@ -30,22 +51,14 @@ echants[:,2] = echants[:,2]*(pmax[2]-pmin[2]) + pmin[2]
 echants[:,3] = echants[:,3]*(pmax[3]-pmin[3]) + pmin[3]
 echants[:,4] = echants[:,4]*(pmax[4]-pmin[4]) + pmin[4]
 echants[:,5] = echants[:,5]*(pmax[5]-pmin[5]) + pmin[5]
+# end of loading sampling
 
-nm = "11"
-rc2000 = pickle.load(open("donnees/rc1000e_m"+nm+"_2kHz.pick","rb"))
-rc5000 = pickle.load(open("donnees/rc1000e_m"+nm+"_5kHz.pick","rb"))
-rc7000 = pickle.load(open("donnees/rc1000e_m"+nm+"_7kHz.pick","rb"))
-rc9000 = pickle.load(open("donnees/rc1000e_m"+nm+"_9kHz.pick","rb"))
-rc11000 = pickle.load(open("donnees/rc1000e_m"+nm+"_11kHz.pick","rb"))
-rc13000 = pickle.load(open("donnees/rc1000e_m"+nm+"_13kHz.pick","rb"))
-RCB = npy.array([ rc2000 , rc5000 , rc7000 , rc9000 , rc11000 , rc13000 ])
-
+# computation of the error, finding the indexes for different errors
 Err = npy.linalg.norm( RCB - RCE.T , axis = 0)
 ind0 = npy.where(Err<0.1)[0]
 ind1 = npy.where(Err<0.06)[0]
 ind2 = npy.where(Err<0.05)[0]
 ind3 = npy.where(Err<0.0475)[0]
-
 for ind in ind1:
     print(*echants[ind,:],RCB[:,ind],Err[ind])
 print("\n")
@@ -55,14 +68,16 @@ print("\n")
 for ind in ind3:
     print(*echants[ind,:],Err[ind],RCB[:,ind])
 print("\n")
-    
+# end of computation of the error
+
+# UNCOMMENT TO PLOT 
 # plt.plot(ind0,Err[ind0],'r+')
 # plt.plot(ind1,Err[ind1],'bo')
 # plt.plot(ind2,Err[ind2],'co')
 # plt.plot(ind3,Err[ind3],'mo')
 # plt.show()
-# #import ipdb ; ipdb.set_trace()
 
+# definition of the misfit function
 zf = 108
 ve = 1523
 rhoE = 1000
@@ -82,46 +97,30 @@ def fonctionOpt(tup):
         rct = MSA.rayon_correlation_verticale(pcapteurs,dc)
         RC = npy.append(RC,rct)
     return npy.linalg.norm(RC - RCE)
+# end of definition of the misfit function
 
 
-for ki,ind in enumerate(ind3):
+# setting multiprocessing
+# 1) the function for multiprocessing
+def eval_ind(ind):
     res = optimize.minimize(fonctionOpt,(echants[ind,0]/100,
                                          echants[ind,2],
                                          echants[ind,3]*1000,
-                                         echants[ind,4]/10),bounds=((15.71,16.25),(0.5,2.0),(0.002*1000,0.005*1000),(0.5,5)),method='L-BFGS-B',options={'gtol':0,'ftol':0.001})
+                                         echants[ind,4]/10),bounds=((15.71,16.25),(0.5,2.0),(0.002*1000,0.005*1000),(0.5,5)),method='L-BFGS-B',options={'gtol':0,'ftol':0.005})
     print(res)
     res = res['x']
     res[0] = res[0]*100
     res[2] = res[2]/1000
     res[3] = res[3]*10
     print(*res)
-    pickle.dump(res,open("res3"+str(ki)+".bin","wb"))
+    pickle.dump(res,open("res_"+str(ind)+".bin","wb"))
+    
 
+# for each set of indexes, processing computation of the optimization
+IND = [ind3,ind2,ind1]
+NUM = [3,2,1]
+for Ind in IND:
+    with Pool(min(20,len(Ind)):
+        Pool.map(eval_ind,Ind)
 
-for ki,ind in enumerate(ind2):
-    res = optimize.minimize(fonctionOpt,(echants[ind,0]/100,
-                                         echants[ind,2],
-                                         echants[ind,3]*1000,
-                                         echants[ind,4]/10),bounds=((15.71,16.25),(0.5,2.0),(0.002*1000,0.005*1000),(0.5,5)),method='L-BFGS-B',options={'gtol':0,'ftol':0.001})
-    print(res)
-    res = res['x']
-    res[0] = res[0]*100
-    res[2] = res[2]/1000
-    res[3] = res[3]*10
-    print(*res)
-    pickle.dump(res,open("res2"+str(ki)+".bin","wb"))
-
-
-for ki,ind in enumerate(ind1):
-    res = optimize.minimize(fonctionOpt,(echants[ind,0]/100,
-                                         echants[ind,2],
-                                         echants[ind,3]*1000,
-                                         echants[ind,4]/10),bounds=((15.71,16.25),(0.5,2.0),(0.002*1000,0.005*1000),(0.5,5)),method='L-BFGS-B',options={'gtol':0,'ftol':0.001})
-    print(res)
-    res = res['x']
-    res[0] = res[0]*100
-    res[2] = res[2]/1000
-    res[3] = res[3]*10
-    print(*res)
-    pickle.dump(res,open("res1"+str(ki)+".bin","wb"))
     
