091c62295a3906cf0a44d1c383bb65c35877f1ec
guilhem <guilhem@sivienn.com>
2019-05-31 13:31:15 +0200

    nettoyage, ajout de commentaires dans SobolIndex

--- sensibilityAnalysis/SobolIndex.py
+++ sensibilityAnalysis/SobolIndex.py
@@ -19,13 +19,21 @@ plt.rcParams.update(params)
 nomsParams = [ "cs" , "rhos" , "epsa" , "sigma" , "lv", "lh" ]
 
 def SobolIndex_computation(cpm):
+    """
+    Given a ChaosPolynomialsModels3 object, compute Sobol index and total Sobol index
+
+    input : cpm, a ChaosPolynomialsModels3 object
+    output : ESI,ESTI : lists of Sobol indexes and total Sobol indexes for each parameter
+    """
     D = npy.sum(cpm.Poids[1:]**2)
     ESI = []
     ESTI = []
-    for ki in range(0,6):
+    # loop on each parameter
+    for ki in range(0,len(cpm.A)): 
         print(ki)
         SI = 0.0
         STI = 0.0
+        # loop on the polynoms of the meta-model
         for kp,mpoly in enumerate(cpm.Polys):
             if (mpoly.degrees[ki] > 0 ) & (npy.sum(mpoly.degrees)==mpoly.degrees[ki]):
                 SI += cpm.Poids[kp]**2
@@ -37,31 +45,52 @@ def SobolIndex_computation(cpm):
         ESTI.append(STI)
     return ESI,ESTI
 
-def SobolIndex_computation_main(CPM,Freqs):
+def SobolIndex_computation_main(CPM,Freqs,name):
+    """
+    Take a set of meta-models to given frequencies, compute Sobol and total Sobol index, and save them
+
+    inputs : CPM, an iterative of ChaosPolynomialsModels3 object
+            Freqs, an iterative of frequency (in Hertzà, each frequency corresponding to a meta-model of CPM
+            name : a string, the "main name" to record files.
+    outputs : none
+    """
     for km,cpm in enumerate(CPM):
         ESI, ESTI = SobolIndex_computation(cpm)
         dico = {"si":ESI,"sti":ESTI}
         df = pds.DataFrame(data=dico,index=nomsParams)
-        df.to_csv("SI_m"+numModele+"_"+str(Freqs[km])+".csv")
+        df.to_csv(name+"_"+str(int(Freqs[km]/1000))+"_kHz.csv") # we add to the main name the frequency number
 
     
-def lecture_resultats(freqs,nomsParams):
+def lecture_resultats(Freqs,nomsParams,name,savefig=True,namefig="SI.png"):
+    """
+    read and plot the Sobol and total Sobol index using bar plots.
+
+    inputs : Freqs.  A iterative of frequencies. For each of its, we have save Sobol Index using SobolIndex_computation_main(...)
+            nomsParams : a iterative of strings , corresponding to the names of the parameters. Can be given in latex.
+            name : a string. The main name to load SobolIndex save with SobolIndex_computation_main(...)
+
+            savefig, optional : a boolean. Set to true if we want to save the figure, false if we want to show()
+            namgefig, optional : a string, the name to save the figure
+
+    outputs : none
+    """
     fig = plt.figure(1,figsize=(17.6,4.95))
-    #AX = [ fig.add_subplot(1,len(freqs),k+1) for k in range(0,len(freqs)) ]
     ax = fig.add_subplot(1,1,1)
     ax.spines['top'].set_color('none')
     ax.spines['bottom'].set_color('none')
     ax.spines['left'].set_color('none')
     ax.spines['right'].set_color('none')
     ax.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
-    AX = [ fig.add_subplot(1,len(freqs),k+1) for k in range(0,len(freqs)) ]
+    AX = [ fig.add_subplot(1,len(Freqs),k+1) for k in range(0,len(Freqs)) ]
     bar_width = 0.35
     index_bar = npy.arange(len(nomsParams))
-    for kf,freq in enumerate(freqs):
-        df = pds.read_csv("./donnees/SI_m"+numModele+"_"+str(freq)+".csv",index_col=0,header=0)
+    for kf,freq in enumerate(Freqs):
+        df = pds.read_csv(name+"_"+str(int(freq/1000))+"_kHz.csv",index_col=0,header=0)
         AX[kf].bar(index_bar,df["sti"], bar_width, color='g',label='Total Sobol index')
         AX[kf].bar(index_bar,df["si"], bar_width, color='r',label='Sobol index')
         AX[kf].set_xticks(index_bar)
+
+        # following three lines : to trace the Sobol index and total Sobol index on two differents columns
         # AX[kf].bar(index_bar,df["si"], bar_width, color='r',label='Sobol index')
         # AX[kf].bar(index_bar + bar_width,df["sti"], bar_width, color='g',label='Total Sobol index')
         # AX[kf].set_xticks(index_bar+bar_width/2)
@@ -71,11 +100,14 @@ def lecture_resultats(freqs,nomsParams):
         #AX[kf].legend()
     #ax.legend(*AX[0].get_legend_handles_labels())
     fig.subplots_adjust(hspace=0.4,bottom=0.1,top=0.9,wspace=0.4,left=0.05,right=0.97)
-    plt.savefig("SI.png")
+    if savefig:
+        plt.savefig(namefig)
+    else:
+        plt.show()
 
     
 if __name__=="__main__":
-    numModele = "4"
+    numModele = "11"
     ##########################
     # chargement des données
     ##########################
@@ -88,5 +120,7 @@ if __name__=="__main__":
     print(*pmax)
     # chargement des méta-modèles
 
-    lecture_resultats([2000,5000,7000,9000,11000,13000],( r"$v_s$" , r"$\rho_s$" , r"$\alpha$" , r"$\sigma$" , r"$l_v$", r"$l_h$" ))
+    #SobolIndex_computation_main(CPM,Freqs)
+
+    lecture_resultats([2000,5000,7000,9000,11000,13000],( r"$c_s$" , r"$\rho_s$" , r"$\alpha$" , r"$\sigma$" , r"$l_v$", r"$l_h$" ),"./donnees/SI",savefig=False)
     #SobolIndex_computation_main(CPM,Freqs)
